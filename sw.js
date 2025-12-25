157948bed2a715710d0915761031ff8cd0bd6993
Maks <maks@Mac.lan>
2025-12-26 04:26:12 +0600

    Initial commit of My-Tracker PWA

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'vape-tracker-v7';
+const ASSETS = [
+  './',
+  './index.html',
+  './login.html',
+  './manifest.json',
+  './icon.png',
+  './vape_bg_new.png',
+  './vape_bg_light.png',
+  './smoke_bg.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        });
+      })
+  );
+});
